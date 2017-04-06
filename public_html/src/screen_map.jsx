7f312327f39ce8fa545d7a3564fabde8e45bd58b
Kayne Ruse <kayneruse@gmail.com>
2017-04-06 20:28:54 +1000

    enabled buttons

--- public_html/src/screen_map.jsx
+++ public_html/src/screen_map.jsx
@@ -4,11 +4,16 @@ import { Button } from 'semantic-ui-react';
 
 import GoogleMap from './google_map.jsx';
 
-import { SCREEN_PROFILE, setScreen } from './actions.jsx';
+import { clearStore, SCREEN_LANDING, SCREEN_PROFILE, setScreen } from './actions.jsx';
 
 class ScreenMap extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  logout() {
+    this.props.clearStore();
+    this.props.setScreen(SCREEN_LANDING);
   }
 
   render() {
@@ -27,8 +32,21 @@ class ScreenMap extends React.Component {
     return (
       <div style={this.props.style}>
         <div style={buttonContainerStyle}>
-          <Button style={buttonStyle} color="violet" floated="left">Logout</Button>
-          <Button style={buttonStyle} color="violet" floated="right">Profile</Button>
+
+          <Button
+            style={buttonStyle}
+            color="violet"
+            floated="left"
+            onClick={() => { this.logout(); }}
+          >Logout</Button>
+
+          <Button
+            style={buttonStyle}
+            color="violet"
+            floated="right"
+            onClick={() => { this.props.setScreen(SCREEN_PROFILE); }}
+          >Profile</Button>
+
         </div>
         <GoogleMap style={{height:"calc(100vh - 24px)"}} />
       </div>
@@ -48,7 +66,8 @@ function mapStateToProps(store) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    setScreen: (screen) => { dispatch(setScreen(screen)); }
+    setScreen: (screen) => { dispatch(setScreen(screen)); },
+    clearStore: () => { dispatch(clearStore()); }
   };
 }
 
